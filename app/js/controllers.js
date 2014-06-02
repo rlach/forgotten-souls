c868cb7525281e8b2d6b9d18eb9271af912daf8e
Rafał Lach <rafal.lach@cohesiva.com>
2014-06-02 16:18:25 +0200

    Added draw and shuffle, improved reset, added some basic game logic, auto reshuffle on final draw, card dummies and basic layout.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -7,6 +7,23 @@ angular.module('myApp.controllers', [])
 	game.start();
 	$scope.decks = game.decks;
 	$scope.treasureTrack = game.treasureTrack;
+	$scope.drawPile = game.drawPile;
+	
+	$scope.draw = function(deck) {
+		game.draw(deck);
+	}
+	
+	$scope.shuffle = function(deck) {
+		game.shuffle(deck);
+	}
+	
+	$scope.getLink = function(deck) {
+		if($scope.drawPile[deck].length === 0) {
+			return 'img/blank.png';
+		} else {
+			return 'img/fs/' + deck + $scope.drawPile[deck][0] + '.png';
+		}
+	}
 	
 	$scope.reset = function() {
 		game.reset();
--- app/js/game/game.js
+++ app/js/game/game.js
@@ -11,11 +11,11 @@ angular.module('myApp.services', ['ngCookies'])
 	function($scope, $log, $cookies) {
 		'use strict';
 
-		var service = {
+		var defaults = {
 			decks: {
-				peril: [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
-				monster: [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
-				exploration: [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+				peril: [ '01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12'],
+				monster: [ '01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12'],
+				exploration: [ '01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12']
 			},
 			treasureTrack: {
 				wounds: 0,
@@ -23,57 +23,120 @@ angular.module('myApp.services', ['ngCookies'])
 				fate: 0,
 				doom: 0,
 				players: 2
-			},
-			start: function() {
-				this.treasureTrack.wounds = this.treasureTrack.wounds + 1;
-			},
-			reset: function() {
-				shuffle(this.decks.peril);
-				shuffle(this.decks.monster);
-				prepareExplorationDeck(this.decks);
 			}
 		};
 
-		if($cookies.treasureTrack !== undefined) {
-			service.treasureTrack = angular.fromJson($cookies.treasureTrack);
-		}
+		var service = {
+			decks: {},
+			drawPile: {
+				peril: [],
+				monster: [],
+				exploration: []
+			},
+			treasureTrack: {},
+			start: function() {
+				service.reset();
 
-		if($cookies.decks !== undefined) {
-			service.decks = angular.fromJson($cookies.decks);
-		}
+				if($cookies.treasureTrack !== undefined) {
+					service.treasureTrack = angular.fromJson($cookies.treasureTrack);
+				}
 
-		$scope.treasureTrack = service.treasureTrack;
-		$scope.decks = service.decks;
+				if($cookies.decks !== undefined) {
+					service.decks = angular.fromJson($cookies.decks);
+				}
 
-		$scope.$watch('treasureTrack', function(newValue, oldValue) {
-			if($scope.treasureTrack !== undefined) {
-				$cookies.treasureTrack = angular.toJson($scope.treasureTrack);
+				if($cookies.drawPile !== undefined) {
+					service.drawPile = angular.fromJson($cookies.drawPile);
+				}
+
+				$scope.treasureTrack = service.treasureTrack;
+				$scope.decks = service.decks;
+				$scope.drawPile = service.drawPile;
+
+				$scope.$watch('treasureTrack', function(newValue, oldValue) {
+					if($scope.treasureTrack !== undefined) {
+						$cookies.treasureTrack = angular.toJson($scope.treasureTrack);
+					}
+				}, true);
+
+				$scope.$watch('decks', function(newValue, oldValue) {
+					if($scope.decks !== undefined) {
+						$cookies.decks = angular.toJson($scope.decks);
+					}
+				}, true);
+
+				$scope.$watch('drawPile', function(newValue, oldValue) {
+					if($scope.drawPile !== undefined) {
+						$cookies.drawPile = angular.toJson($scope.drawPile);
+					}
+				}, true);
+			},
+			reset: function() {
+				//angular.extend(this.decks, defaults.decks);
+				angular.copy(defaults.decks,this.decks);
+				emptyPile(this.drawPile.peril);
+				emptyPile(this.drawPile.monster);
+				emptyPile(this.drawPile.exploration);
+				//angular.extend(this.treasureTrack, defaults.treasureTrack);
+				angular.copy(defaults.treasureTrack, this.treasureTrack);
+
+				shuffle(this.decks.peril);
+				shuffle(this.decks.monster);
+				prepareExplorationDeck(this.decks);
+			},
+			shuffle: function(deck) {
+				if(typeof deck === 'string') {
+					try {
+						//angular.extend(this.decks[deck], defaults.decks[deck]);
+						angular.copy(defaults.decks[deck], this.decks[deck]);
+						shuffle(this.decks[deck]);
+						emptyPile(this.drawPile[deck]);
+					}
+					catch(err) {
+						$log.error("ERROR in shuffle deck", err);
+					}
+				}
+			},
+			draw: function(deck) {
+				if(typeof deck === 'string') {
+					try {
+						if(this.decks[deck].length === 0) {
+							if(deck === 'exploration') { 
+								return;
+							}
+							this.shuffle(deck);
+						}
+						var drawnCard = this.decks[deck].shift();
+						this.drawPile[deck].unshift(drawnCard);
+					}
+					catch(err) {
+						$log.error("ERROR in draw deck", err);
+					}
+				}
 			}
-		}, true);
-
-		$scope.$watch('decks', function(newValue, oldValue) {
-			if($scope.decks !== undefined) {
-				$cookies.decks = angular.toJson($scope.decks);
-			}
-		}, true);
+		};
 
 		function prepareExplorationDeck(targetDecks) {
-			var explorationTail = [ 5, 6, 7, 8, 9, 10, 11, 12];
+			var explorationTail = [ '05', '06', '07', '08', '09', '10', '11', '12'];
 			shuffle(explorationTail);
 			var explorationA = explorationTail.slice(0, 3);
-			explorationA.push(2);
+			explorationA.push('02');
 			var explorationB = explorationTail.slice(3, 6);
-			explorationB.push(3);
+			explorationB.push('03');
 			var explorationC = explorationTail.slice(6, 8);
-			explorationC.push(4);
+			explorationC.push('04');
 			shuffle(explorationA);
 			shuffle(explorationB);
 			shuffle(explorationC);
-			var explorationComplete = [1];
+			var explorationComplete = ['01'];
 			explorationComplete = explorationComplete.concat(explorationA);
 			explorationComplete = explorationComplete.concat(explorationB);
 			explorationComplete = explorationComplete.concat(explorationC);
 			targetDecks.exploration = explorationComplete;
+		}
+
+		function emptyPile(array) {
+			array.length = 0;
 		}
 
 		function shuffle(array) {
